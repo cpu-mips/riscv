a6c2b738b5b69cb13640c14b4c773c4bfb0d10ec
Class_Account <cs150-cx@c125m-8.EECS.Berkeley.EDU>
2014-10-25 23:59:35 -0700

    Adding UATransmit from lab6

--- hardware/src/UATransmit.v
+++ hardware/src/UATransmit.v
@@ -6,19 +6,79 @@ module UATransmit(
   input         DataInValid,
   output        DataInReady,
 
-  output        SOut
+  output    reg    SOut
 );
-    // for log2 function
-    `include "util.vh"
+  // for log2 function
+  `include "util.vh"
 
-    //--|Parameters|--------------------------------------------------------------
-    parameter   ClockFreq         =   100_000_000;
-    parameter   BaudRate          =   115_200;
+  //--|Parameters|--------------------------------------------------------------
 
-    // See diagram in the lab guide
-    localparam  SymbolEdgeTime    =   ClockFreq / BaudRate;
-    localparam  ClockCounterWidth =   log2(SymbolEdgeTime);
+  parameter   ClockFreq         =   100_000_000;
+  parameter   BaudRate          =   115_200;
 
-    // Copy your UART transmitter implementation from Lab 6 to here.
+  // See diagram in the lab guide
+  localparam  SymbolEdgeTime    =   ClockFreq / BaudRate;
+  localparam  ClockCounterWidth =   log2(SymbolEdgeTime);
+
+  //--|Solution|----------------------------------------------------------------
+   wire 	SymbolEdge;
+   //wire 	Sample;
+   wire 	Start;
+   wire 	TXRunning;
+   wire [9:0] 	TXShift;
+   reg [3:0] 	BitCounter;
+   reg [ClockCounterWidth-1:0] ClockCounter;
+   reg 			       hold;
+
+   //assign Start = !TXRunning;
+   assign Start = DataInValid;
+   
+   assign  SymbolEdge   = (ClockCounter == SymbolEdgeTime - 1);
+   assign TXShift = (DataInValid || hold)?{1'b1,DataIn,1'b0}:10'b0;
+   assign DataInReady = !TXRunning;
+   assign  TXRunning     = BitCounter != 4'd0;
+   
+   always @ (posedge Clock) begin
+      ClockCounter <= (Start || Reset || SymbolEdge) ? 0 : ClockCounter + 1;
+   end
+
+   always @ (posedge Clock) begin
+    if (Reset) begin
+      BitCounter <= 0;       
+    end else if (DataInValid) begin
+      BitCounter <= 10;
+    end else if (SymbolEdge && TXRunning) begin
+      BitCounter <= BitCounter - 1;
+    end
+    
+   end
+
+   always @ (posedge Clock) begin
+      if (!Reset)begin
+	 if (!TXRunning)
+	   hold<=0;
+	 else
+	   hold<=1;
+      end
+      else
+	hold <= 0;
+      
+      
+   end
+   
+
+   always @(posedge Clock) begin
+    if (TXRunning)begin
+       //SOut <= 1;
+       
+      SOut <= TXShift[10-BitCounter];
+       //TXShift <= {SOut, TXShift[9:1]};
+       
+    end
+  end
+
+   //always @ (posedge Clock) begin
+     // if (!TXRunning && DataInValid)
+   
 
 endmodule
