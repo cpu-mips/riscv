d41e5c05646630b37fea59c3fde26b8436c0f52d
Niraj Amalkanti <neji49@gmail.com>
2014-11-03 14:18:35 -0800

    Wrote implementation for MemControl.v

--- hardware/src/MemControl.v
+++ hardware/src/MemControl.v
@@ -12,6 +12,8 @@
 //    Uart_recv: Receive signal for UART
 //-----------------------------------------------------------------------------
 
+`include Opcode.vh
+
 module MemControl(
     input [6:0] Opcode,
     input [2:0] Funct3,
@@ -20,5 +22,72 @@ module MemControl(
     output [3:0] Imem_enable,
     output [3:0] Uart_trans,
     output Uart_recv);
+
+    reg [3:0] dmem_reg, imem_reg, mask_reg, uart_trans_reg
+    reg uart_recv_reg;
+
+    assign Dmem_enable = dmem_reg;
+    assign Imem_enable = imem_reg;
+    assign Uart_trans = uart_trans_reg;
+    assign Uart_recv = uart_recv_reg;
+
+    always@(*)
+    begin
+        case(Opcode)
+            `OPC_LOAD:
+            begin
+                dmem_reg = 4'b000;
+                imem_reg = 4'b000;
+                uart_trans_reg = 4'b000;
+                if (4'b1000 == A[31:28])
+                begin
+                    uart_recv_reg = 1'b1;
+                end
+                else
+                begin
+                    uart_recv_reg = 1'b0;
+                end
+            end
+            `OPC_STORE:
+            begin
+                case (Funct3)
+                    `FNC_SB:mask_reg = 4'b0001;
+                    `FNC_SH:mask_reg = 4'b0011;
+                    default:mask_reg = 4'b1111;
+                endcase
+                if (1'b0 == A[31] && 1'b1 == A[28])
+                begin
+                    dmem_reg = mask_reg;
+                end
+                else
+                begin
+                    dmem_reg = 4'b000;
+                end
+                if (1'b0 == A[31] && 1'b1 == A[29])
+                begin
+                    imem_reg = mask_reg;
+                end
+                else
+                begin
+                    imem_reg = 4'b000;
+                end
+                if (4'b1000 == A[31:28])
+                begin
+                    uart_trans_reg = mask_reg;
+                end
+                else
+                begin
+                    uart_trans_reg = 4'b000;
+                end
+            end
+        endcase
+        default:
+        begin
+            dmem_reg = 4'b000;
+            imem_reg = 4'b000;
+            uart_trans_reg = 4'b000;
+            uart_recv_reg = 1'b0;
+        end
+    end
 
 endmodule
