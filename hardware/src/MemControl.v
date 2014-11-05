0593f7749a7e251e3ab1b4ac82e4587da8bf34e5
Niraj Amalkanti <neji49@gmail.com>
2014-11-04 22:51:42 -0800

    Updating MemControl

--- hardware/src/MemControl.v
+++ hardware/src/MemControl.v
@@ -52,7 +52,23 @@ module MemControl(
             begin
                 case (Funct3)
                     `FNC_SB:mask_reg = 4'b0001;
+                    begin
+                        case (A[1:0])
+                            00:mask_reg = 4'b0001;
+                            01:mask_reg = 4'b0010;
+                            10:mask_reg = 4'b0100;
+                            11:mask_reg = 4'b1000;
+                            default:mask_reg = 4'bx;
+                        endcase
+                    end
                     `FNC_SH:mask_reg = 4'b0011;
+                    begin
+                        case (A[1:0])
+                            00:mask_reg = 4'b0011;
+                            10:mask_reg = 4'b1100;
+                            default:mask_reg = 4'bx;
+                        endcase
+                    end
                     default:mask_reg = 4'b1111;
                 endcase
                 if (1'b0 == A[31] && 1'b1 == A[28])
