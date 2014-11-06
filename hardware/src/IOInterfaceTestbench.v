5fad5c03390de7e3c0174776f531e00b905cd4e9
Niraj Amalkanti <neji49@gmail.com>
2014-11-06 02:50:55 -0800

    Added address for control

--- hardware/src/IOInterfaceTestbench.v
+++ hardware/src/IOInterfaceTestbench.v
@@ -104,6 +104,8 @@ module IOInterfaceTestbench();
         Addr = 32'h80000008;
         REFout = rd2;
         #(2 * Cycle)
+        Addr 32'h80000000;
+        (#Cycle);
         while (1'b0 == recieve_out[0])
         begin
             #(Cycle);
