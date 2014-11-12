61cc3805ce5a4b9ab7e6b4dcff95f0d62a0e74ee
Class_Account <cs150-cx@c125m-4.EECS.Berkeley.EDU>
2014-11-12 00:46:28 -0800

    fixing edge case for data hazards

--- hardware/src/HazardController.v
+++ hardware/src/HazardController.v
@@ -2,46 +2,46 @@
 `include "Opcode.vh"
 
 module HazardController(input stall, input [6:0]OpcodeW, input [6:0] OpcodeX, 
-	input [4:0] rd, input[4:0] rs1, input[4:0] rs2, input diverge, 
-	output reg CWE2, output reg  ForwardA, output reg ForwardB, output reg delayW, 
-        output reg delayX);
+	input [4:0] rd, input[4:0] rs1, input[4:0] rs2, input diverge, input [13:0] PC_X,
+        input [13:0] PC_W, output reg CWE2, output reg  ForwardA, output reg ForwardB, 
+        output reg delayW, output reg delayX);
 
 always @(*) begin
     if (stall == 0) begin
     case (OpcodeW) 
        `OPC_ARI_RTYPE: begin
-	  ForwardA = (rd == rs1 && rd != 0)?1:0;
-	  ForwardB = (rd == rs2 &&  rd != 0)?1:0;
+	  ForwardA = (rd == rs1 && rd != 0 && PC_X != PC_W)?1:0;
+	  ForwardB = (rd == rs2 &&  rd != 0 && PC_X != PC_W)?1:0;
 	  delayW = 0;
 	  CWE2 = 1;    
        end
        `OPC_ARI_ITYPE: begin
-	  ForwardA = (rd == rs1 && rd != 0)?1:0;
-	  ForwardB = (rd == rs2 && rd != 0)?1:0;
+	  ForwardA = (rd == rs1 && rd != 0 && PC_X != PC_W)?1:0;
+	  ForwardB = (rd == rs2 && rd != 0 && PC_X != PC_W)?1:0;
 	  delayW = 0;
 	  CWE2 = 1;
        end 
       `OPC_LUI: begin
-          ForwardA = (rd == rs1 && rd != 0)?1:0;
-          ForwardB = (rd == rs2 && rd != 0)?1:0;
+          ForwardA = (rd == rs1 && rd != 0 && PC_X != PC_W)?1:0;
+          ForwardB = (rd == rs2 && rd != 0 && PC_X != PC_W)?1:0;
           delayW = 0;
           CWE2 = 1;
       end
       `OPC_AUIPC: begin
-          ForwardA = (rd == rs1 && rd != 0)?1:0; 
-          ForwardB = (rd == rs2 && rd != 0)?1:0;
+          ForwardA = (rd == rs1 && rd != 0 && PC_X != PC_W)?1:0; 
+          ForwardB = (rd == rs2 && rd != 0 && PC_X != PC_W)?1:0;
           delayW = 0;
           CWE2 = 1;
       end
       `OPC_JAL: begin
-          ForwardA = (rd == rs1 && rd != 0)?1:0;
-          ForwardB = (rd == rs2 && rd != 0)?1:0;
+          ForwardA = (rd == rs1 && rd != 0 && PC_X != PC_W)?1:0;
+          ForwardB = (rd == rs2 && rd != 0 && PC_X != PC_W)?1:0;
           delayW = 0;
           CWE2 = 1;
       end
       `OPC_JALR: begin
-          ForwardA = (rd == rs1 && rd != 0)?1:0;
-          ForwardB = (rd == rs2 && rd != 0)?1:0;
+          ForwardA = (rd == rs1 && rd != 0 && PC_X != PC_W)?1:0;
+          ForwardB = (rd == rs2 && rd != 0 && PC_X != PC_W)?1:0;
           delayW = 0;
           CWE2 = 1;
       end
