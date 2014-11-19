6abfc191596c1a9c4a98c96ee95fd9951de48122
Class_Account <cs150-cx@c125m-17.EECS.Berkeley.EDU>
2014-11-18 19:44:18 -0800

    Disabling WE for branches

--- hardware/src/HazardController.v
+++ hardware/src/HazardController.v
@@ -3,10 +3,11 @@
 
 module HazardController(input stall, input [6:0]OpcodeW, input [6:0] OpcodeX, 
 	input [4:0] rd, input[4:0] rs1, input[4:0] rs2, input diverge, input [13:0] PC_X,
-        input [13:0] PC_W, output noop, output reg CWE2, output reg ForwardA, output 
+        input [13:0] PC_W, output noop, output CWE2, output reg ForwardA, output 
 	reg ForwardB, output reg delayW);
-reg noopX, noopW;
+reg noopX, noopW, CWE2_temp;
 assign noop = noopX | noopW;
+assign CWE2 = (OpcodeX == `OPC_BRANCH) ? 0:CWE2_temp;
 always @(*) begin
     //if (stall == 0) begin
     case (OpcodeW) 
@@ -14,56 +15,56 @@ always @(*) begin
 	  ForwardA = (rd == rs1 && rd != 0 && PC_X != PC_W)?1:0;
 	  ForwardB = (rd == rs2 &&  rd != 0 && PC_X != PC_W)?1:0;
 	  delayW = 0;
-	  CWE2 = 1; 
+	  CWE2_temp = 1; 
           noopW = 0;   
        end
        `OPC_ARI_ITYPE: begin
 	  ForwardA = (rd == rs1 && rd != 0 && PC_X != PC_W)?1:0;
 	  ForwardB = (rd == rs2 && rd != 0 && PC_X != PC_W)?1:0;
 	  delayW = 0;
-	  CWE2 = 1;
+	  CWE2_temp = 1;
           noopW = 0;
        end 
       `OPC_LUI: begin
           ForwardA = (rd == rs1 && rd != 0 && PC_X != PC_W)?1:0;
           ForwardB = (rd == rs2 && rd != 0 && PC_X != PC_W)?1:0;
           delayW = 0;
-          CWE2 = 1;
+          CWE2_temp = 1;
 	  noopW = 0;
       end
       `OPC_AUIPC: begin
           ForwardA = (rd == rs1 && rd != 0 && PC_X != PC_W)?1:0; 
           ForwardB = (rd == rs2 && rd != 0 && PC_X != PC_W)?1:0;
           delayW = 0;
-          CWE2 = 1;
+          CWE2_temp = 1;
           noopW = 0;
       end
       `OPC_JAL: begin
           ForwardA = (rd == rs1 && rd != 0 && PC_X != PC_W)?1:0;
           ForwardB = (rd == rs2 && rd != 0 && PC_X != PC_W)?1:0;
           delayW = 0;
-          CWE2 = 1;
+          CWE2_temp = 1;
           noopW = 0;
       end
       `OPC_JALR: begin
           ForwardA = (rd == rs1 && rd != 0 && PC_X != PC_W)?1:0;
           ForwardB = (rd == rs2 && rd != 0 && PC_X != PC_W)?1:0;
           delayW = 0;
-          CWE2 = 1;
+          CWE2_temp = 1;
           noopW = 0;
       end
       `OPC_LOAD: begin
 	  ForwardA = 0;
 	  ForwardB = 0;
 	  delayW = ((rd == rs1 || rd == rs2) && rd != 0) ? 1:0;
-	  CWE2 = ((rd == rs1 || rd == rs2) && rd != 0) ? 0:1;
+	  CWE2_temp = ((rd == rs1 || rd == rs2) && rd != 0) ? 0:1;
           noopW = ((rd == rs1 || rd == rs2) && rd != 0) ? 1:0;  
       end
       default:begin
 	  ForwardA = 0;
 	  ForwardB = 0;
 	  delayW = 0;
-	  CWE2 = 1;
+	  CWE2_temp = 1;
           noopW = 0;
       end
     endcase // case (OpcodeW)
